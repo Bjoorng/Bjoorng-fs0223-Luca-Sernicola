b9f1497e8598322f480639d3ea273c1a7ad7acbc
Bjoorng <luca.sernicola@gmail.com>
2023-04-03 18:05:14 +0200

    stuff

--- m1/s2/g1/homework/js/script2.js
+++ m1/s2/g1/homework/js/script2.js
@@ -3,9 +3,11 @@ var n = 15
 var t =21
 
 function math(){
-    if(onclick){
-
-        n + t
+    if(confirm("choose wisely")){
+    
+    }else{
+        
+        txt = "I see you're a man of culture aswell"
     }
     document.getElementById("result").innerHTML = n + t
 }
