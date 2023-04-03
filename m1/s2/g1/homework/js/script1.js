35f7227704f17d4cf0ca1e4c275f82db1692685a
Bjoorng <luca.sernicola@gmail.com>
2023-04-03 17:01:31 +0200

    buttons logs and math

---
+++ m1/s2/g1/homework/js/script1.js
@@ -0,0 +1,29 @@
+function dewit(){
+    var txt
+    if (confirm("press a button!")) {
+        txt = "Animals as Leaders. Check log for songs to listen to..."
+        console.log ('Phisycal Education')
+        console.log ('The Woven Web')
+        console.log ('Cafo')
+        console.log ('An Infinite Regression')
+        console.log ('Ka$kade')
+    }else{
+        txt = "Sadge"
+    }
+    document.getElementById("boh").innerHTML = txt
+}
+
+function doit(){
+    var txt
+    if (confirm("press a button!")){
+        txt = "Periphery. Check log for songs to listen to..."
+        console.log ('Alpha')
+        console.log ('The Gods Must Be Crazy')
+        console.log ('Omega')
+        console.log ('The Way The News Goes')
+        console.log ('Wax Wings')
+    }else{
+        txt = "Sadge"
+    }
+    document.getElementById("sad").innerHTML = txt
+}
---
+++ m1/s2/g1/homework/js/script2.js
@@ -0,0 +1,11 @@
+
+var n = 15
+var t =21
+
+function math(){
+    if(onclick){
+        
+        n + t
+    }
+    document.getElementById("quickmath").innerHTML = n + t
+}
