9342c11783b79dd679521ea6f7e95c0dff1732ff
Bjoorng <luca.sernicola@gmail.com>
2023-04-05 18:58:56 +0200

    fix

--- m1/s2/g3/personal training/shipping.js
+++ m1/s2/g3/personal training/shipping.js
@@ -1,4 +1,20 @@
+{
 let cart = Number(prompt('dimmi quanto hai speso e ti dirò se è gratis'))
+/*let discount = cart*0.8*/
+let free = 50
+let ship = 10
+
+{
+    if(cart >= free){
+        document.write(`il tuo carrello è ${cart}£. la tua spedizione è gratuita`)
+    }else{
+        document.write(`il tuo carrello è inferiore a ${free}£. Con la spedizione il totale sarà ${cart + ship}£`)
+    }
+}
+}
+
+{
+let cart = Number(prompt('dimmi quanto hai speso e ti dirò se è gratis con sconto'))
 let discount = cart*0.8
 let free = 50
 let ship = 10
@@ -7,6 +23,7 @@ let ship = 10
     if(discount >= free){
         document.write(`il tuo carrello è ${cart}£. la tua spedizione è gratuita`)
     }else{
-        document.write(`il tuo carrello è inferiore a ${free}£. il totale sarà ${discount + ship}£`)
+        document.write(`il tuo carrello è inferiore a ${free}£. Con la spedizione il totale sarà ${discount + ship}£`)
     }
+}
 }
