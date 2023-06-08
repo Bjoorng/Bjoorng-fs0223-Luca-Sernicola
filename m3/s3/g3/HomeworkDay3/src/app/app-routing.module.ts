94d1b3f8f9644c589d18257f03d00f250df82b6c
Bjoorng <luca.sernicola@gmail.com>
2023-06-08 11:27:45 +0200

    stuff

---
+++ m3/s3/g3/HomeworkDay3/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/module/home.module').then(m => m.HomeModule)
+  },
+  { path: 'customer',
+  loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
