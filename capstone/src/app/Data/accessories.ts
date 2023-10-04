c16b02ebe1f7ffc9f540c9d09d8f5ea8e455c7d7
Luca Sernicola <125129925+Bjoorng@users.noreply.github.com>
2023-10-04 02:41:23 +0200

    fix

--- capstone/src/app/Data/accessories.ts
+++
@@ -1,8 +0,0 @@
-export const accessories = [
-  {
-    imageUrl: 'https://m.media-amazon.com/images/I/51k-qOBhPgL.jpg',
-    title: 'Needle',
-    price: '50,21',
-    description: 'Needle for your record player',
-  }
-]
--- capstone/src/app/Data/merch.ts
+++
@@ -1,9 +0,0 @@
-export const merch = [
-  {
-    imageUrl: 'https://images.stockx.com/images/Travis-Scott-x-McDonalds-Fry-II-T-Shirt-Brown.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1606346571',
-    title: 'T-Shirt',
-    artist: 'Travis Scott',
-    price: '40,00',
-    description: '80% cotton, 20% polyester',
-  },
-]
